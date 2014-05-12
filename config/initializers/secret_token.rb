4802fc0f8c927916ed517cc0bf65989aae180630
Newbsylberry <chris.s.mccarthy@gmail.com>
2014-05-11 22:45:49 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fitnessapp::Application.config.secret_key_base = '007970be0568e8354a1cadafce053a2e5862d22d8f8098f3a665ddb307beab728344c67646845f4c9928eb65bbcc0e161bb1b4f1431d4b3763a011ce2514569a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fitnessapp::Application.config.session_store :cookie_store, key: '_fitnessapp_session'
